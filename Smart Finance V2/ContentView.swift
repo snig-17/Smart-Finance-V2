bc448d7bf8c8c0de138f8862ad4f1b4f06e8fd52
snig-17 <snigdha.17.tiwari@icloud.com>
2025-08-09 17:51:56 +0530

    basic dashboard UI added

--- Smart Finance V2/ContentView.swift
+++ Smart Finance V2/ContentView.swift
@@ -9,15 +9,71 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-  
+    //connect to coredata
+    @Environment(\.managedObjectContext) private var viewContext
+    //fetch transactions from database
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Transaction.transactionDate, ascending: false)], animation: .default)
+    private var transactions: FetchedResults<Transaction>
     
     var body: some View {
-        Text("Hello World!")
+        NavigationView{
+            ScrollView{
+                VStack(spacing: 20){
+                        balanceCardSection
+                        recentTransactionSection
+                        quickStatsSection
+                    
+                }
+                .padding()
+            }
+            .navigationTitle("Smart Finance")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing){
+                        addButton
+                }
+            }
         }
     }
-
-
-
-#Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    
 }
+
+extension ContentView {
+    private var balanceCardSection : some View {
+        Text("Balance Card (coming soon...)")
+            .frame(height:100)
+            .frame(maxWidth: .infinity)
+            .background(Color.blue.opacity(0.1))
+            .cornerRadius(12)
+    }
+    private var recentTransactionSection : some View {
+        Text("Transactions (coming soon...)")
+            .frame(height: 200)
+            .frame(maxWidth: .infinity)
+            .background(Color.green.opacity(0.1))
+            .cornerRadius(12)
+    }
+    private var quickStatsSection : some View {
+        Text("Stats (coming soon...)")
+            .frame(height: 100)
+            .frame(maxWidth: .infinity)
+            .background(Color.orange.opacity(0.1))
+            .cornerRadius(12)
+    }
+    private var addButton : some View {
+        Button(action: addTransaction) {
+            Image(systemName: "plus.circle.fill")
+                .font(.title2)
+                .foregroundColor(.blue)
+        }
+    }
+    private func addTransaction() {
+        print("Add transaction tapped")
+    }
+}
+
+    #Preview {
+        ContentView()
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
