22adc81a831642a032a05c9bcb6e86486da307b9
snig-17 <snigdha.17.tiwari@icloud.com>
2025-08-12 18:17:36 +0530

    transaction model created

--- Smart Finance V2/Transactions/Models/Transaction+Extensions.swift
+++ Smart Finance V2/Transactions/Models/Transaction+Extensions.swift
@@ -5,3 +5,12 @@
 //  Created by Snigdha Tiwari  on 12/08/2025.
 //
 
+import Foundation
+import CoreData
+
+// MARK: - transaction extensions
+
+extension Transaction {
+    // MARK: - computed properties
+    
+}
---
+++ Smart Finance V2/Transactions/Models/TransactionModel.swift
@@ -0,0 +1,31 @@
+//
+//  TransactionModel.swift
+//  Smart Finance V2
+//
+//  Created by Snigdha Tiwari  on 12/08/2025.
+//
+
+import Foundation
+
+struct TransactionModel {
+    let id: UUID
+    var amount: Double
+    var description: String
+    var date: Date
+    var categoryName: String
+    
+    // MARK: - computed properties
+    
+    var isIncome: Bool {
+        return amount > 0
+    }
+    var isExpense: Bool {
+        return amount < 0
+    }
+    var formattedAmount: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter.string(from: NSNumber(value: abs(amount))) ?? "$0.00"
+    }
+
+}
